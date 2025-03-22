64bd7d61e73246f6d5f5a9915ce68473e33fabc6
TKatana <turjohalder41@gmail.com>
2025-03-22 17:42:38 +0600

    so many changes were made

--- main.js
+++ main.js
@@ -17,3 +17,48 @@ function scrollBrands(direction) {
         container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
     }
 }
+
+
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ctx = document.getElementById('polarChart');
+  if (ctx) {
+    new Chart(ctx, {
+      type: 'polarArea',
+      data: {
+      labels: ['Solar Charger', 'Eco Bulb', 'Reusable Batteries', 'Smart Thermostat', 'Green Speaker'],
+      datasets: [{
+        label: 'Popularity',
+        data: [12, 19, 7, 15, 10],
+        backgroundColor: [
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(8, 150, 244, 0.6)',
+        'rgba(240, 181, 30, 0.6)',
+        'rgba(32, 232, 232, 0.6)',
+        'rgba(87, 16, 229, 0.6)'
+        ],
+      }]
+      },
+      options: {
+      responsive: true,
+      layout: {
+        padding: 20
+      },
+      plugins: {
+        legend: {
+        position: 'right',
+        labels: {
+          padding: 10
+        }
+        }
+      },
+      scales: {
+        r: {
+        beginAtZero: true
+        }
+      }
+      }
+    });
+  }
+});
